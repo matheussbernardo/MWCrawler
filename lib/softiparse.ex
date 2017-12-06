38262ea6626fa894c44f9a7d740a00a1b73a2852
Matheus Bernardo <matheussbernardo@gmail.com>
2017-12-06 01:15:22 -0200

    Getting courses prerequisites and zipping on a keyword list

--- lib/softiparse.ex
+++ lib/softiparse.ex
@@ -1,23 +1,32 @@
 defmodule Parser do
-  @moduledoc """
-  Documentation for Parser.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Parser.hello
-      :world
-
-  """
-  def hello do
+  defp list_of_courses do
     url = "https://matriculaweb.unb.br/graduacao/fluxo.aspx?cod=6360"
     body = HTTPoison.get!(url).body
 
-    raw_list_of_courses = body |> Floki.find("div.body.table-responsive table a") |> Floki.text(sep: "-") |> String.split("-")
-    list_of_courses = Enum.map(raw_list_of_courses, fn(course) -> String.trim_trailing(course) end)
-    
+    raw_list_of_courses = body 
+                          |> Floki.find("div.body.table-responsive table a") 
+                          |> Floki.text(sep: "-") 
+                          |> String.split("-")
+    Enum.map(raw_list_of_courses, fn(course) -> String.trim_trailing(course) |> String.to_atom  end)
+
+  end
+
+  defp links_of_courses do
+    url = "https://matriculaweb.unb.br/graduacao/fluxo.aspx?cod=6360"
+    body = HTTPoison.get!(url).body
+    body |> Floki.find("div.body.table-responsive table a") |> Floki.attribute("href") 
+  end
+  
+  defp get_dependency(url) do
+    body = HTTPoison.get!("https://matriculaweb.unb.br/#{url}").body
+    body |> Floki.find("#datatable tr:nth-child(6) a") |> Floki.text
+  end
+  
+  defp dependencies_links do
+    Task.async_stream(links_of_courses(), fn(link) -> get_dependency(link) end)
+  end
+
+  def courses_and_dependencies do
+    courses = Enum.zip(list_of_courses(), dependencies_links())
   end
 end
